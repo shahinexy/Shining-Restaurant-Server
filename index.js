d42c4591099d62581721e835999b09b8d49e8823
shahinexy <shahinexy@gmail.com>
2024-05-21 00:23:30 +0600

    delete method use for cart delete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app= express()
 const port = process.env.PORT || 5000
 
@@ -54,6 +54,13 @@ async function run() {
       const newCart = req.body;
       const result = await cartCollection.insertOne(newCart)
       res.send(result)
+    })
+
+    app.delete('/carts/:id' , async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await cartCollection.deleteOne(query)
+        res.send(result)
     })
 
     await client.db("admin").command({ ping: 1 });
